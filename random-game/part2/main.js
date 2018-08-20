17e87d1f5cdf165f4b0e2c97437beb6b2a462324
Filip Martinsson <martinsson.filip@gmail.com>
2018-08-20 19:12:36 +0200

    Update main.js
    missing scores

--- random-game/part2/main.js
+++ random-game/part2/main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 	var endpoint = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
 	var common = nem.model.objects.create("common")("password", "5d533ad1c22fb6237b0ed3471a7ed41a845119543ca4fb7baf47711c4c2549e8");
 	var mosaicDefinitions = nem.model.objects.get("mosaicDefinitionMetaDataPair");
-
+	var scores;
   function roll() {
 		var random = Math.floor(Math.random()*1000001);
 		console.log(random);
